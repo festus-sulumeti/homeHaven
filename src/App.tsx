ff4b7248e446968ebd55818b6b8dc861a3d88cd6
festus-sulumeti <sulumetifestus@outlook.com>
2025-11-02 22:05:20 +0300

    feat: sign up route

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { Routes, Route } from  'react-router-dom'
 import Hero from './components/HeroSection/Hero'
 import Login from './Pages/Login'
+import Signup from './Pages/Signup'
+
 
 function App() {
  
@@ -10,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Hero />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </>
   )
